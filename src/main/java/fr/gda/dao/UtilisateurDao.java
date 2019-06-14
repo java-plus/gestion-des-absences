2d78ddd182836b1d7b04ef6ff6faed38cc964845
Cecile Peyras <peyrascecile@gmail.com>
2019-06-14 10:18:48 +0200

    gda : authentification

--- src/main/java/fr/gda/dao/UtilisateurDao.java
+++ src/main/java/fr/gda/dao/UtilisateurDao.java
@@ -1,5 +1,15 @@
 package fr.gda.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import fr.gda.connexion.ConnexionManager;
+import fr.gda.exception.TechnicalException;
+
 /**
  * Classe qui gère les action des utilisateurs
  * 
@@ -15,9 +25,47 @@ public class UtilisateurDao {
 	 *            : email saisi par l'utilisateur
 	 * @return
 	 */
-	public boolean validerMdp(String email) {
-		return false;
+	public boolean validerMdp(String email, String password) {
+		Connection conn = ConnexionManager.getInstance();
+		PreparedStatement statement = null;
+		ResultSet curseur = null;
 
+		try {
+			conn.setAutoCommit(false);
+			statement = conn.prepareStatement("SELECT * FROM utilisateur WHERE mail = ?");
+			statement.setString(1, email);
+			curseur = statement.executeQuery();
+
+			if (curseur.next()) {
+
+				String mdp = curseur.getString("mdp");
+				DigestUtils.sha512(mdp);
+
+				if (mdp.equals(password)) {
+					return true;
+				}
+
+			}
+
+			conn.commit();
+		} catch (SQLException e) {
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+				throw new TechnicalException("Le rollback n'a pas fonctionné", e);
+			}
+			throw new TechnicalException("L'ajout ne s'est pas fait", e);
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+			} catch (SQLException e) {
+
+				throw new TechnicalException("La fermeture ne s'est pas faite", e);
+			}
+		}
+		return false;
 	}
 
 }
