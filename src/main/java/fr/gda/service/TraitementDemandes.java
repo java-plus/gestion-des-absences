ca149003b23e63003ba17f50364a5c751e98e0fc
Patrice Allardin <patrice.allardin@free.fr>
2019-06-14 14:34:00 +0200

    GDA : Traitement de nuit

--- src/main/java/fr/gda/service/TraitementDemandes.java
+++ src/main/java/fr/gda/service/TraitementDemandes.java
@@ -18,8 +18,22 @@ public class TraitementDemandes {
 	}
 
 	public static void main(String[] args) {
+		// Création de Dao pour les demandes de congés au statut Initiale
 		AbsenceParPersonneDao absenceParPersonneDao = new AbsenceParPersonneDao();
+		// Récupération des demandes dans l'ordre des dates des demandes
 		List<AbsenceParPersonne> absenceAuStatutInitiale = absenceParPersonneDao.lireDemandesEnStatutInitiale();
+
+		// * Pour les congés payés, congés sans solde et RTT employés:
+
+		// * S'il reste assez de jours pour le type d'absence demandé, la
+		// demande passe au statut EN_ATTENTE_VALIDATION et le compteur du
+		// collaborateur est décrémenté du nb de jours correspondant. Un mail de
+		// demande de validation est envoyé au manager de l'employé.
+		// * S'il ne reste pas assez de jours alors la demande est rejetée: elle
+		// passe au statut REJETEE.
+		// * Pour les demandes de type RTT employeur alors la demande passe au
+		// statut VALIDEE et l'ensemble des employés se voient retirer un jour
+		// de RTT.
 
 		for (AbsenceParPersonne abs : absenceAuStatutInitiale)
 			System.out.println(abs.getStatut());
