5b01147863276628916ea9d2d9d72bd92f98e33c
Bilel KHARBECHE <bilelkharbeche71@gmail.com>
2019-06-17 17:07:50 +0200

    Mise a jour férié et rtt emp

--- src/main/java/fr/gda/controller/FerieRttEmpController.java
+++ src/main/java/fr/gda/controller/FerieRttEmpController.java
@@ -37,4 +37,9 @@ public class FerieRttEmpController extends HttpServlet {
 
 		}
 	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	}
 }
