1a0ccb304a1c604657cba0413b615a11ec9092fd
Eloi Turpin <eloi.turpin@gmail.com>
2019-06-26 14:47:30 +0200

    fin de la fin des fin

--- src/main/java/fr/gda/dao/AbsenceParPersonneDao.java
+++ src/main/java/fr/gda/dao/AbsenceParPersonneDao.java
@@ -1,10 +1,14 @@
+package fr.gda.dao;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import fr.gda.connexion.ConnexionManager;
